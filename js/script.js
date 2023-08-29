710c950d63a7d3240c6271cd09bbb706a08544c8
wvll3f <wallef720p@gmail.com>
2023-08-29 09:50:37 -0300

    accordion inserido

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import ScrollSuave from "./modules/scroll-suave";
+import Accordion from "./modules/accordion";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init();
