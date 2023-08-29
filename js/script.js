dee30058cd0d3ab1c58282d48f42919189e0b47a
wvll3f <wallef720p@gmail.com>
2023-08-29 14:40:26 -0300

    adicionando mais algumas funcionalidades js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 import ScrollSuave from "./modules/scroll-suave";
 import Accordion from "./modules/accordion";
 import TabNav from "./modules/tabnav";
+import Modal from "./modules/modal";
+import ScrollAnima from "./modules/scroll-anima";
+import Tooltip from "./modules/tooltip"
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -9,4 +12,13 @@ const accordion = new Accordion('[data-anime="accordion"] dt')
 accordion.init();
 
 const tabnav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
-accordion.init();
+tabnav.init();
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
