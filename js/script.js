020b5d5c9545ddc5e312a20118ec3e98a8229713
wvll3f <wallef720p@gmail.com>
2023-08-29 10:08:21 -0300

    TabNav adicionado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 import ScrollSuave from "./modules/scroll-suave";
 import Accordion from "./modules/accordion";
+import TabNav from "./modules/tabnav";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
 const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init();
+
+const tabnav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
 accordion.init();
